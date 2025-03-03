f82c08efbbe3f6634b8ab35773e46deb11d16bc0
F-Aguilar12 <fercho140903@gmail.com>
2025-03-03 11:51:43 -0600

    Create actividad en clase.py

---
+++ actividad en clase.py
@@ -0,0 +1,32 @@
+#Número se asigna un valor a una variable
+numero = 10
+
+#Se imprime el valor de la variable número, ej. 10
+print("El número asignado a la variable número es: ", numero)
+
+#""""
+int = 21
+
+string = "Fernando"
+
+booleano_1 = True
+
+booleano_2 = False
+#
+print("Mi edad es: ", int)
+print("Mi nombre es : ", string)
+print("Soy mayor de edad?: ", booleano_1)
+print("Soy malo en el fornai?: ", booleano_2)
+
+#"""Sentencia if - es una condicion de sentencia""""
+if numero < 100:
+    print("El número es menor que 100")
+else:
+    print ("El número es mayor que 100")
+
+
+#"""""
+if numero == 3:
+    print("Tu número es impar")
+else:
+    print("Tu número es par")
