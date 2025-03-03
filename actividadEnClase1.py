2413c3b4fa290dddc070b2c66021e809a06b306d
F-Aguilar12 <fercho140903@gmail.com>
2025-03-03 11:53:27 -0600

    ActividadEnClase1.py

---
+++ actividadEnClase1.py
@@ -0,0 +1,44 @@
+print("Hola que tal, guapisimos, aqui vegetta 777")
+print("vania")
+print("fernando")
+print("eduardo")
+print("valeria")
+print("alan")
+print("kevin")
+print("gio")
+print("zaira")
+print("hiram")
+
+""" Vamos a Concatenar """
+nombre = "Fernando"
+apellido = "Aguilar"
+print("Hola mi nombre es: ", nombre)
+print("Mi apellido es: ", apellido)
+print("My name is: ", nombre, apellido)
+
+""" Concatenación con diferentes tipos de datos """
+mi_edad= 21
+mi_nombre= "Fernando"
+mi_apellido_paterno= "Aguilar"
+mi_apellido_materno= "Cinseros"
+soy_alumno= True
+soy_maestro= False
+
+print("Hola, mi nombre es:",mi_nombre, mi_apellido_paterno, mi_apellido_materno, ", tengo", mi_edad, "años, soy estudiante", soy_alumno)
+
+""" uso de type - Averigua que tipo de dato recibes """
+print(type(mi_edad)) # <class 'int'>
+print(type(soy_alumno)) # <class 'boot'>
+print(type(mi_nombre)) # <class 'str'>
+
+
+""" Operaciones Matematicas """
+numero_1= 21
+numero_2= 10
+
+print(numero_1 + numero_2)
+print(numero_1 - numero_2)
+print(numero_1 * numero_2)
+print(numero_1 / numero_2)
+print(numero_1 % numero_2)
+
